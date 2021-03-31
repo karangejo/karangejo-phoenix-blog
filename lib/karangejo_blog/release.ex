af8c17021c3758d1badb6246b1ef4aa8bc263408
karangejo <gabriel.g.gras@gmail.com>
2021-03-31 13:19:19 +0800

    added post insertions for release

--- lib/karangejo_blog/release.ex
+++ lib/karangejo_blog/release.ex
@@ -1,18 +1,38 @@
 defmodule KarangejoBlog.Release do
-    @app :karangejo_blog 
-
-    def migrate do
-          for repo <- repos() do
+      @app :karangejo_blog 
+      
+      def migrate do
+            for repo <- repos() do
                   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-                end
-        end
+            end
+      end
 
-    def rollback(repo, version) do
+      def insert_posts do 
+            files =
+                  Application.app_dir(:karangejo_blog, "priv/repo/markdown/*.md") 
+                  |> Path.wildcard
+                  
+            Enum.map(files, fn file ->
+                  case File.read(file) do
+                    {:ok, content} ->
+                      post_name =
+                        Path.basename(file)
+                        |> String.replace(".md","")
+                        |> String.replace("_"," ")
+                      
+                      Repo.insert!(%Post{name: post_name, content: content})
+                    _ ->
+                      IO.puts("could not insert file : " ++ file)
+                  end
+            end)
+      end
+
+      def rollback(repo, version) do
           {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
-        end
+      end
 
-    defp repos do
+      defp repos do
           Application.load(@app)
           Application.fetch_env!(@app, :ecto_repos)
-        end
+      end
   end
