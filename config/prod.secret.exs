ab1946d2ffe44c0811368d336ad4f41a530d537e
karangejo <gabriel.g.gras@gmail.com>
2021-03-31 06:46:11 +0200

    release

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||
+  System.get_env("KARANGEJO_DB_URL") ||
     raise """
     environment variable DATABASE_URL is missing.
     For example: ecto://USER:PASS@HOST/DATABASE
@@ -25,7 +25,7 @@ secret_key_base =
 
 config :karangejo_blog, KarangejoBlogWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "4343"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
@@ -35,7 +35,7 @@ config :karangejo_blog, KarangejoBlogWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :karangejo_blog, KarangejoBlogWeb.Endpoint, server: true
+config :karangejo_blog, KarangejoBlogWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
